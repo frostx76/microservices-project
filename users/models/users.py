d9fc87a3efc24486e692c91c42acb2e4ce3f5b93
Timur <tfzakirov@edu.hse.ru>
2025-07-20 05:40:07 +0300

    Добавили users в compose + описали db для users

---
+++ users/models/users.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Field
+from datetime import date
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)  # id из auth
+    email: str = Field(index=True, unique=True)
+    full_name: str | None = None
+    is_active: bool = True
+    bio: str | None = Field(default=None, max_length=500)
+    birthdate: date | None = None
+    phone_number: str | None = Field(default=None, max_length=20)
+    address: str | None = None
